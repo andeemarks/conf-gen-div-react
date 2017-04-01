3141548888bee8d0fa72b96f6932f7b98a695956
Andy Marks <amarks@thoughtworks.com>
2017-04-01 17:27:09 +1100

    Conf name is most important - it should be first where applicable

--- components/Callouts/Callouts.js
+++ components/Callouts/Callouts.js
@@ -20,7 +20,7 @@ class Callouts extends React.Component {
         <div className="row">
           <div className="col-sm-4">
             <div id={s.title}>Biggest recent improver</div>
-            <div id={s.body}>+36%<br/>1st Conf<br/>21% (2016) -> 57% (2017)</div>
+            <div id={s.body}><strong>1st Conf</strong><br/>+36%<br/>21% (2016) -> 57% (2017)</div>
           </div>
           <div className="col-sm-4" id={s.nbrConfAtParity}>
             <div id={s.title}>Number of confs >= 50%</div>
@@ -28,7 +28,7 @@ class Callouts extends React.Component {
           </div>
           <div className="col-sm-4">
             <div id={s.title}>Best performer</div>
-            <div id={s.body}>58%<br/>Code Mania<br/>2016<br/>Auckland, New Zealand</div>
+            <div id={s.body}><strong>Code Mania 2016</strong><br/>58%<br/>Auckland, New Zealand</div>
           </div>
         </div>
       </div>
