db901467f358402baf33d23d8f3ba3f595c11831
Andy Marks <amarks@thoughtworks.com>
2017-04-01 17:22:18 +1100

    Hard-coded data for callout categories

--- components/Callouts/Callouts.js
+++ components/Callouts/Callouts.js
@@ -20,15 +20,15 @@ class Callouts extends React.Component {
         <div className="row">
           <div className="col-sm-4">
             <div id={s.title}>Biggest recent improver</div>
-            <div id={s.body}>FOO</div>
+            <div id={s.body}>+36%<br/>1st Conf<br/>21% (2016) -> 57% (2017)</div>
           </div>
-          <div className="col-sm-4">
+          <div className="col-sm-4" id={s.nbrConfAtParity}>
             <div id={s.title}>Number of confs >= 50%</div>
-            <div id={s.body}>BAR</div>
+            <div id={s.body}>6</div>
           </div>
           <div className="col-sm-4">
             <div id={s.title}>Best performer</div>
-            <div id={s.body}>BLECH</div>
+            <div id={s.body}>58%<br/>Code Mania<br/>2016<br/>Auckland, New Zealand</div>
           </div>
         </div>
       </div>
