abd56b6daf5250a45dc0455726060ef6d2e8bc58
Andy Marks <amarks@thoughtworks.com>
2017-10-07 17:34:27 +1100

    Displaying overall average diversity %age

--- components/Callouts/Callouts.js
+++ components/Callouts/Callouts.js
@@ -7,6 +7,10 @@ function diversityAtParityOrGreater(conf) {
   return conf.diversityPercentage >= 50;
 }
 
+function diversityAccumulator(accumulator, conf) { 
+  return accumulator + conf.diversityPercentage; 
+}
+
 function diversitySorter(confA, confB) {
   if (confA.diversityPercentage < confB.diversityPercentage) {
     return 1;
@@ -26,7 +30,8 @@ class Callouts extends React.Component {
     this.state = {
       confs: props.confs,
       bestPerformer: props.confs.sort(diversitySorter)[0],
-      numberOfConfs: props.confs.length
+      numberOfConfs: props.confs.length,
+      averageDiversity: props.confs.reduce(diversityAccumulator, 0) / props.confs.length
     };
   }
 
@@ -49,6 +54,16 @@ class Callouts extends React.Component {
           <div className="col-sm-3" id={s.nbrConfAtParity}>
             <div className={s.title}>Number of confs >= 50%</div>
             <div className={s.pop}>{(this.state.confs.filter(diversityAtParityOrGreater)).length}</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-3">
+            <div className={s.title}>Average f:m%</div>
+            <div className={s.pop}>{numbro(this.state.averageDiversity).format('0')}%</div>
+          </div>
+          <div className="col-sm-3">
+            <div className={s.title}>Average f:m% (2017)</div>
+            <div className={s.body}><strong>{this.state.bestPerformer.name} ({this.state.bestPerformer.year})</strong><br/>{numbro(this.state.bestPerformer.diversityPercentage).format('0')}%<br/>{this.state.bestPerformer.location}</div>
           </div>
         </div>
       </div>
