59418942d312ce6ce504c9129b3d2b048ad67b9b
Andy Marks <amarks@thoughtworks.com>
2017-10-07 11:23:39 +1100

    Adding callout for number of confs tracked

--- components/Callouts/Callouts.js
+++ components/Callouts/Callouts.js
@@ -35,15 +35,19 @@ class Callouts extends React.Component {
     return (
       <div className={s.container}>
         <div className="row">
-          <div className="col-sm-4">
+          <div className="col-sm-3">
+            <div className={s.title}>Conferences tracked</div>
+            <div className={s.body}><strong>{this.state.confs.length}</strong></div>
+          </div>
+          <div className="col-sm-3">
             <div className={s.title}>Best performer</div>
             <div className={s.body}><strong>{this.state.bestPerformer.name} ({this.state.bestPerformer.year})</strong><br/>{numbro(this.state.bestPerformer.diversityPercentage).format('0')}%<br/>{this.state.bestPerformer.location}</div>
           </div>
-          <div className="col-sm-4">
+          <div className="col-sm-3">
             <div id={s.title}>Biggest recent improver</div>
             <div id={s.body}><strong>1st Conf</strong><br/>+36%<br/>21% (2016) -> 57% (2017)</div>
           </div>
-          <div className="col-sm-4" id={s.nbrConfAtParity}>
+          <div className="col-sm-3" id={s.nbrConfAtParity}>
             <div id={s.title}>Number of confs >= 50%</div>
             <div id={s.body}>{(this.state.confs.filter(diversityAtParityOrGreater)).length}</div>
           </div>
